990619429def25b741393054ba8bee4cbe4278f1
Kalle Sillanpää <kalle.sillanpaa@gmail.com>
2021-12-17 19:02:42 +0200

    add card.html add model.py add flashcards_db.json

--- flashcards.py
+++ flashcards.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
-
+from model import db
 app = Flask(__name__)
 
+welcome_message="Welcome to Flask page!"
 
 @app.route('/')
 def hello_world():
-    welcome_message="Welcome to Flask page!"
+    global welcome_message
     return render_template("welcome.html", welcome_message=welcome_message)
 
 
+@app.route('/card')
+def show_card():
+    card = db[0]
+    return render_template("card.html", card=card)
