d28b5984d4002659af24f4a6e93b27722f09a91d
Kalle Sillanpää <kalle.sillanpaa@gmail.com>
2021-12-18 11:18:14 +0200

    edit model.py : added save_db() add add_card.html : added form with submit edit flashcards.py : added add_card -route with saving to db

--- flashcards.py
+++ flashcards.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, abort
-from model import db
+from flask import Flask, render_template, abort, request, redirect, url_for
+from model import db, save_db
+
 app = Flask(__name__)
 
-welcome_message="Welcome to Flask page!"
+welcome_message = "Welcome to Flask page!"
+
 
 @app.route('/')
 def welcome():
@@ -13,7 +15,18 @@ def welcome():
 def show_card(index):
     try:
         card = db[index]
-        return render_template("card.html", card=card, index=index, max_index=len(db)-1)
+        return render_template("card.html", card=card, index=index, max_index=len(db) - 1)
     except IndexError:
         abort(404)
 
+
+@app.route('/add_card', methods=["GET", "POST"])
+def add_card():
+    if request.method == "POST":
+        card = {"question": request.form['question'],
+                "answer": request.form['answer']}
+        db.append(card)
+        save_db()
+        return redirect(url_for('show_card', index=len(db)-1))
+    else:
+        return render_template('add_card.html')
